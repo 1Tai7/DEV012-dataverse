a9f4d11ca471887fb24d8022376651e43d961448
Ilce <ilsemilena1706@gmail.com>
2023-10-06 18:56:46 -0500

    correccion de la dataset, faltan mas arreglos

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { example } from './dataFunctions.js';
-import { renderItems } from './view.js';
+import { example } from "./dataFunctions.js";
+import { renderItems } from "./view.js";
 
-import data from './data/dataset.js';
+import data from "./data/dataset.js";
+data.filter((element) => console.log(element.URL));
 
 console.log(example, renderItems(data), data);
