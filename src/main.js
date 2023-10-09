aa9e1b088135699f3ad126f8f5a59541746319e0
Ilce <ilsemilena1706@gmail.com>
2023-10-09 09:21:43 -0500

    cree funcion apra que las tarjetas aparecieran en la web

--- src/main.js
+++ src/main.js
@@ -2,6 +2,16 @@ import { example } from "./dataFunctions.js";
 import { renderItems } from "./view.js";
 
 import data from "./data/dataset.js";
-data.filter((element) => console.log(element.URL));
+
+function addElement() {
+  const img = document.createElement("img");
+  const ul = document.querySelector("ul");
+  const item = document.createElement("li");
+  let algo = "";
+  for (let index = 0; index < data.length; index++) {
+    ul.innerHTML += `<li itemscope class= "card"><img src="${data[index].URL}" width= "100px" alt=""> <p>${data[index].name}</p></li> `;
+  }
+}
+addElement();
 
 console.log(example, renderItems(data), data);
