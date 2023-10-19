6863cf5b464cccb4b2a641e4aea31d6fd8d83f11
Vaneval <vaneval.dese@gmail.com>
2023-10-19 12:36:42 -0500

    se creran las funciones puras para ordenar y generar estadísticas. Nota: Faltan elementos de las funciones filtrar y render items para que ordenar funcione correctamente

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -7,3 +7,34 @@ export const example = () => {
 export const anotherExample = () => {
   return [];
 };
+
+export function sortData(data, sortBy) {
+  const sortedData = [...data];
+
+  if (sortBy === "asc") {
+    sortedData.sort((a, b) => a.name.localeCompare(b.name));
+  } else if (sortBy === "desc") {
+    sortedData.sort((a, b) => b.name.localeCompare(a.name));
+  } else if (sortBy === "ascAge") {
+    sortedData.sort((a, b) => a.facts.Age - b.facts.Age);
+  } else if (sortBy === "descAge") {
+    sortedData.sort((a, b) => b.facts.Age - a.facts.Age);
+  } else if (sortBy === "ascHeight") {
+    sortedData.sort((a, b) => a.facts.Height - b.facts.Height);
+  } else if (sortBy === "descHeight") {
+    sortedData.sort((a, b) => b.facts.Height - a.facts.Height);
+  } else {
+    sortedData.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  return sortedData;
+}
+
+export const computeStats = (data, value) => {
+  const dataFiltered = data.reduce((sum, character) => {
+    if (character.species.affiliation === value) sum++;
+    return sum;
+  }, 0);
+
+  return dataFiltered;
+};
--- src/main.js
+++ src/main.js
@@ -1,34 +1,57 @@
+import data from "./data/dataset.js";
 import { example } from "./dataFunctions.js";
 import { renderItems } from "./view.js";
+import { sortData } from "./dataFunctions.js";
+import { computeStats } from "./dataFunctions.js";
 
-import data from "./data/dataset.js";
+const stats = document.getElementById("stats");
+stats.innerHTML = "Resultados de tu selección: " + data.length;
+  
+const applyFilterButton = getElementById("applyFilter")
+applyFilterButton.addEventListener("click", ()=> {
+  const selectOption = select.options[select.selectedIndex];
+  const buttonClear = document.querySelector('button[data-testid="button-clear"]');
+  const sortBySelector = document.querySelector('select[name="sortBy"]');
+  const sortBy = sortBySelector.value;
+  const amountOfCharacteres = computeStats(species, affiliation);
 
-function addElement() {
-  const img = document.createElement("img");
-  const ul = document.querySelector("ul");
-  const item = document.createElement("li");
-  let algo = "";
-  for (let index = 0; index < data.length; index++) {
-    ul.innerHTML += `<li itemscope class= "card"><img src="${data[index].URL}" width= "100px" alt=""> <p>${data[index].name}</p></li> `;
+  const sortedData = sortData(dataFiltered, sortBy);
+  //falta renderItems :c
+
+  //cod Ilce :3
+  function addElement() {
+    const img = document.createElement("img");
+    const ul = document.querySelector("ul");
+    const item = document.createElement("li");
+    let algo = "";
+    for (let index = 0; index < data.length; index++) {
+      ul.innerHTML += `<li itemscope class= "card">
+      <img src="${data[index].URL}"  alt="img"><div class="textCard">
+      <span>${data[index].name}</span>
+      <span>${data[index].shortDescription}</span>
+      <span>Especie: ${data[index].facts.species}</span>
+      <span>Afiliación: ${data[index].facts.affiliation}</span>
+      <span>Estatura: ${data[index].facts.Height} m</span>
+      <span>Edad: ${data[index].facts.Age}</span></div></li> `;
+    }
   }
-}
-addElement();
-const select = document.querySelector('[data-testid="select-filter"]');
-let filterForSpecies = "";
-select.addEventListener("change", function () {
-  let selectOption = this.options[select.selectedIndex];
-  filterForSpecies = selectOption.value;
-  console.log(selectOption.value);
+  addElement();
+  const select = document.querySelector('[data-testid="select-filter"]');
+  let filterForSpecies = "";
+  select.addEventListener("change", function () {
+    let selectOption = this.options[select.selectedIndex];
+    filterForSpecies = selectOption.value;
+    console.log(selectOption.value);
+  });
+  const button = document.querySelector("button");
+  button.addEventListener("click", function () {
+    const filterSpecies = data.filter(
+      (character) => character.facts.species === filterForSpecies
+    );
+    console.log(filterSpecies);
+  });
+  
+  console.log(example, renderItems(data), data);
+ 
 });
-const button = document.querySelector("button");
-button.addEventListener("click", function () {
-  const fascendente = data.filter(
-    (character) => character.facts.species === filterForSpecies
-  ); const fdescendente = data.filter(
-    (character) => character.facts.species === filterForSpecies
-  );
 
-  console.log(filterSpecies);
-});
-
-console.log(example, renderItems(data), data);
